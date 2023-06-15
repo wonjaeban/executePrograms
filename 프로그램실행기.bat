f2ed1639d0ad0556fc1e7258e6e6a4fa52b5c804
반 원재 <wonjae0709@jinhakapply.com>
2023-06-15 09:01:24 +0900

    feat: 로그 출력 제거

--- 프로그램실행기.bat
+++ 프로그램실행기.bat
@@ -1,3 +1,4 @@
+@echo off
 start "" "C:\Users\wonjae0709\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Microsoft Teams" & ^
 start "" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Microsoft SQL Server Tools 18\Microsoft SQL Server Management Studio 18" & ^
 start "" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Git\Git Bash" & ^ 
